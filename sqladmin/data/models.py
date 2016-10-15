3b70c7fbc6f72fe5ba589593e7714a925adb8b3f
richard <rxc@umich.edu>
2016-10-15 02:35:12 -0400

    add req file

--- sqladmin/data/models.py
+++ sqladmin/data/models.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -11,9 +9,6 @@ from django.db import models
 #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from __future__ import unicode_literals
-
-from django.db import models
 
 
 class AnswerT(models.Model):
