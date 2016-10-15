9b7c6b70136d416c3d5f72c473f4bea7b68fea96
vmelandri12 <virginiamelandri@gmail.com>
2016-10-15 05:02:43 -0400

    Questions and Answers
    questions and answers retrieved

--- ResCalls.java
+++ ResCalls.java
@@ -1,46 +1,82 @@
+import java.awt.PageAttributes.MediaType;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import org.json.*;
+import java.util.ArrayList;
 
 import javax.ws.rs.*;
-
 
 @Path("/choices/")
 public class ResCalls {
 	Connection conn = null;
 	Statement stmt = null;
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		ResCalls r = new ResCalls();
-		r.getLocations();
+		r.getQuestion();
 	}
-	private static void printMap(Map<String, Integer> mp) {
-	    Iterator it = mp.entrySet().iterator();
-	    while (it.hasNext()) {
-	        HashMap.Entry pair = (HashMap.Entry)it.next();
-	        System.out.println(pair.getKey() + " = " + pair.getValue());
-	        it.remove(); // avoids a ConcurrentModificationException
-	    }
-	}
-//	@GET
-//	@Path("/getLocations/")
-//	@Produces("application/json")
-	public String getLocations() {
-		Map<String, Integer> hash = new HashMap<String, Integer> ();
 
-		String sqL = "SELECT location FROM user_t";
+	private static void printMap(Map<String, Integer> mp) {
+		Iterator it = mp.entrySet().iterator();
+		while (it.hasNext()) {
+			HashMap.Entry pair = (HashMap.Entry) it.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue());
+			it.remove(); // avoids a ConcurrentModificationException
+		}
+	}
+
+
+	// @GET
+	// @Path("/getLocations/")
+	// @Produces(MediaType.APPLICATION_JSON)
+	public void getGeneral(String name) {
+		Map<String, Integer> hash = new HashMap<String, Integer>();
+
+		String sqL = "SELECT " + name + " FROM user_t";
 		try {
 			ResultSet rs = getConnection().executeQuery(sqL);
 
 			while (rs.next()) {
 				// Retrieve by column name
-				String location = rs.getString("location");
-				if (hash.containsKey(location)) {
-					int num = hash.get(location);
-					hash.put(location, num+1);
-				}else{
-					hash.put(location, 1);
+				String ans = rs.getString(name);
+				if (hash.containsKey(ans)) {
+					int num = hash.get(ans);
+					hash.put(ans, num + 1);
+				} else {
+					hash.put(ans, 1);
+				}
+			}
+
+			JSONObject returnedJSON = new JSONObject(hash);
+			// printMap(hash);
+			System.out.println(returnedJSON);
+			closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void getLocations() {
+		getGeneral("location");
+	}
+
+	public void getAge() {
+		Map<String, Integer> hash = new HashMap<String, Integer>();
+
+		String sqL = "SELECT age FROM user_t";
+		try {
+			ResultSet rs = getConnection().executeQuery(sqL);
+
+			while (rs.next()) {
+				// Retrieve by column name
+				String ans = rs.getString("age");
+				if (hash.containsKey(ans)) {
+					int num = hash.get(ans);
+					hash.put(ans, num + 1);
+				} else {
+					hash.put(ans, 1);
 				}
 			}
 			printMap(hash);
@@ -48,10 +84,43 @@ public class ResCalls {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+
 	}
-	
-	
+
+	public void getGender() {
+		getGeneral("gender");
+	}
+
+	public void getRace() {
+		getGeneral("race");
+	}
+
+	public void getQuestion() {
+		Map<String, ArrayList<String>> qhash = new HashMap<String, ArrayList<String>>();
+
+		String sqQ = "SELECT question_id, q_text FROM question_t";
+		try {
+			ResultSet rs = getConnection().executeQuery(sqQ);
+
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("question_id");
+				String q_str = rs.getString("q_text");
+				String ans = "SELECT * FROM answer_t WHERE question_id = " + id;
+				ResultSet rsAns = getConnection().executeQuery(ans);
+				ArrayList<String> ansList = new ArrayList<String>();
+				while (rsAns.next()) {
+					ansList.add(rsAns.getString("answer"));
+				}
+				qhash.put(q_str, ansList);
+			}
+			JSONObject returnedJSON = new JSONObject(qhash);
+			System.out.println(returnedJSON);
+			closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private Statement getConnection() {
 
